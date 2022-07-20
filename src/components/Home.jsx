b0fd4294c63aa379dd152e925601f35563cd5470
Tarun Verma <34982557+ta-verma@users.noreply.github.com>
2022-07-21 00:25:04 +0530

    Change Cred

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -58,7 +58,7 @@ const Home = () => {
                     className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
                     Login
                   </button>
-                  <p className="text-sm font-semibold mt-2 pt-1 mb-0">
+                  {/* <p className="text-sm font-semibold mt-2 pt-1 mb-0">
                     Don't have an account?
                     <a
                       href="#!"
@@ -66,7 +66,7 @@ const Home = () => {
                       {" "}
                       Register
                     </a>
-                  </p>
+                  </p> */}
                 </div>
               </form>
             </div>
